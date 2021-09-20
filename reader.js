0b23203457747cd25bf5578c5423e9ebbab34086
IvanAguado22 <100405871@alumnos.uc3m.es>
2021-09-20 13:48:59 +0200

    move function v1

--- reader.js
+++ reader.js
@@ -25,9 +25,9 @@ for ( let i = 3; i < dataInput.length; i++) {
     }
     if(dataInput[i] == 'L' || dataInput[i] == 'R' || dataInput[i] == 'F' ){
         robotsInst.push(dataInput[i]);
-    }
-    if(dataInput[i] == '\n'){
-        robotsInst.push('$');
+        if(dataInput[i+1] == '\r'){
+            robotsInst.push('$');
+        }
     }
 }
 
@@ -35,19 +35,86 @@ console.log(robotsCoor);
 console.log(robotsOrien);
 console.log(robotsInst);
 console.log(numRobots);
+let j = 0;
+let k = 0;
+let n = 0;
 
-/*function move (coor, orien, inst, num){
+move(robotsCoor, robotsOrien, robotsInst, numRobots);
+
+console.log(robotsCoor);
+console.log(robotsOrien);
+console.log(robotsInst);
+console.log(numRobots);
+
+function output(){
+    console.log()
+}
+
+function move (coor, orien, inst, num){
     for (let i = 0; i < num; i++ ){
-
+        while(robotsInst[j] != '$'){
+            if(robotsInst[j] == 'R'){
+                switch(robotsOrien[n]){
+                    case 'N':
+                        robotsOrien[n] = 'E';
+                        break;
+                    case 'S':
+                        robotsOrien[n] = 'W';
+                        break;
+                    case 'E':
+                        robotsOrien[n] = 'S';
+                        break;
+                    case 'W':
+                        robotsOrien[n] = 'N';
+                        break;
+                }                   
+            }          
+            else if(robotsInst[j] == 'L'){
+                switch(robotsOrien[n]){
+                    case 'N':
+                        robotsOrien[n] = 'W';
+                        break;
+                    case 'S':
+                        robotsOrien[n] = 'E';
+                        break;
+                    case 'E':
+                        robotsOrien[n] = 'N';
+                        break;
+                    case 'W':
+                        robotsOrien[n] = 'S';
+                        break;
+                }                                 
+            }
+            else if(robotsInst[j] == 'F'){
+                switch(robotsOrien[n]){
+                    case 'N':
+                        robotsCoor[k + 1] += 1;
+                        break;
+                    case 'S':
+                        robotsCoor[k + 1] -= 1;
+                        break;
+                    case 'E':
+                        robotsCoor[k] += 1;
+                        break;
+                    case 'W':
+                        robotsCoor[k] -= 1;
+                        break;
+                }           
+            }
+        j++;       
+        }
+    j++;
+    n++;
+    k += 2;
     }
-    return console.log("holi");
-}*/
+}
 
  function isNumeric(str) {
   if (typeof str != "string") return false // we only process strings!  
   return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
          !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
+
 
 
 
