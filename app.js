ed3ef532bbd58fe1eaa24b92462c3900f8c8a96f
IvanAguado22 <100405871@alumnos.uc3m.es>
2021-09-21 23:13:15 +0200

    code rewriting

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
-var fs = require('fs');
-var input = fs.readFileSync('input.txt', 'utf-8');
 
-let toChar = function(str) {
-    return [...str]
-};
-
-var dataInput = toChar(input);
-console.log(dataInput);
-
-var gridX = parseInt(dataInput[0]);
-var gridY = parseInt(dataInput[2]);
+// VARIABLES //
 const robotsCoor = [];
 const robotsOrien = [];
 const robotsInst = [];
 var numRobots = 0;
-
-for ( let i = 3; i < dataInput.length; i++) {
-    if(isNumeric(dataInput[i])){
-        robotsCoor.push(parseInt(dataInput[i]));
-    }
-    if(dataInput[i] == 'N' || dataInput[i] == 'S' || dataInput[i] == 'E' || dataInput[i] == 'W' ){
-        robotsOrien.push(dataInput[i]);
-        numRobots++;
-    }
-    if(dataInput[i] == 'L' || dataInput[i] == 'R' || dataInput[i] == 'F' ){
-        robotsInst.push(dataInput[i]);
-        if(dataInput[i+1] == '\r'){
-            robotsInst.push('$');
-        }
-    }
-}
-
+var gridX;
+var gridY;
 let j = 0;
 let k = 0;
 let n = 0;
-
-move(robotsCoor, robotsOrien, robotsInst, numRobots);
-
 let coorIndex = 0;
 let orienIndex = 0;
 
+// MAIN CODE
+
+storeData();
+move();
 output();
+
+// FUNCTIONS //
+function readInput(){
+    var fs = require('fs');
+    var input = fs.readFileSync('input.txt', 'utf-8');
+
+    let toChar = function(str) {
+        return [...str]
+    };
+
+    var dataInput = toChar(input);
+    return dataInput;
+}
+
+function storeData(){
+    var dataInput = readInput();
+    gridX = parseInt(dataInput[0]);
+    gridY = parseInt(dataInput[2]);
+
+    for ( let i = 3; i < dataInput.length; i++) {
+        if(isNumeric(dataInput[i])){
+            robotsCoor.push(parseInt(dataInput[i]));
+        }
+        if(dataInput[i] == 'N' || dataInput[i] == 'S' || dataInput[i] == 'E' || dataInput[i] == 'W' ){
+            robotsOrien.push(dataInput[i]);
+            numRobots++;
+        }
+        if(dataInput[i] == 'L' || dataInput[i] == 'R' || dataInput[i] == 'F' ){
+            robotsInst.push(dataInput[i]);
+            if(dataInput[i+1] == '\r'){
+                robotsInst.push('$');
+            }
+        }
+    }
+}
 
 function output(){
     for(let i = 0; i < numRobots; i++){
@@ -50,8 +61,8 @@ function output(){
     }    
 }
 
-function move (coor, orien, inst, num){
-    for (let i = 0; i < num; i++ ){
+function move (){
+    for (let i = 0; i < numRobots; i++ ){
         while(robotsInst[j] != '$'){
             if(robotsInst[j] == 'R'){
                 switch(robotsOrien[n]){
@@ -109,10 +120,9 @@ function move (coor, orien, inst, num){
     }
 }
 
- function isNumeric(str) {
-  if (typeof str != "string") return false // we only process strings!  
-  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+function isNumeric(str) {
+    if (typeof str != "string") return false   
+    return !isNaN(str) && !isNaN(parseFloat(str)) 
 }
 
 
